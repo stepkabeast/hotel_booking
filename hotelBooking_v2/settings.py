d01de6719fce7ea291b7432b60cb1a4a1a4ff8b9
Stepan <stepak038@gmail.com>
2025-03-17 20:05:20 +0800

    CRUD операции

--- hotelBooking_v2/settings.py
+++ hotelBooking_v2/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'modules.Infrastructure.orm.db.apps.DbConfig',
-    'crud_app.apps.CrudAppConfig'
 ]
 
 MIDDLEWARE = [
