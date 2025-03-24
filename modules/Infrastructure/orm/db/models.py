3e857d6e171618c3c1cf30b7a365e77ad7db8ef7
Stepan <stepak038@gmail.com>
2025-03-24 13:16:10 +0800

    update models

--- modules/Infrastructure/orm/db/models.py
+++ modules/Infrastructure/orm/db/models.py
@@ -1,28 +1,42 @@
 from django.db import models
-from modules.Entities.Room import Room
+
 
 class Customer(models.Model):
     GENDERS = [
         ('MALE', 'Male'),
-        ('FEMALE', 'Female'),
+        ('FEMALE', 'Female')
     ]
 
-    gender = models.CharField(max_length=10, choices=GENDERS)
-    age = models.IntegerField()
-    passport_id = models.JSONField() # Используем JSONField для словаря
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"{self.name} {self.surname}"
+    age = models.IntegerField()
+    passport_id = models.JSONField()
+    gender = models.CharField(max_length=10, choices=GENDERS)
 
 
 class Room(models.Model):
-    categories = Room.CATEGORIES
+    CATEGORIES = [
+        ('BASE', 'Base'),
+        ('LUX', 'Lux'),
+        ('PREMIUM', 'Premium')
+    ]
+
+    number = models.CharField(max_length=10)
+    category = models.CharField(max_length=20, choices=CATEGORIES)
+
 
 class Booking(models.Model):
-    #name = Customer.name
-    #surname = Customer.surname
+    STATUS_CHOICES = [
+        ('waiting', 'Waiting'),
+        ('settled', 'Settled'),
+        ('extended', 'Extended'),
+        ('evicted', 'Evicted')
+    ]
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     check_in_date = models.DateField()
     check_out_date = models.DateField()
-    room = models.CharField(max_length=10, choices=Room.categories)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    breakfast = models.BooleanField(default=False)
+    product_intolerance = models.JSONField(null=True, blank=True)
