7b2ad04c1a25068d8d5dab36e05c97fefa389367
Stepan <stepak038@gmail.com>
2025-03-27 15:06:32 +0800

    fix: переделал ajax для вывода списка свободных номеров для конкретной категории номера

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -4,18 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     button.addEventListener('click', async () => {
         try {
-            const response = await fetch('/check_rooms/', {
+            const selectedCategory = document.getElementById('room-category-select').value;
+            console.log("Selected Category:", selectedCategory);
+            const url = '/check_rooms/?category=' + encodeURIComponent(selectedCategory);
+            const response = await fetch(url, {
                 method: 'GET',
             });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
             const data = await response.json();
 
-            roomsList.innerHTML = '';
-            data.forEach((room_id) => {
+            roomsList.innerHTML = ''; // Очищаем содержимое списка
+
+            // Выводим каждое значение room_number
+            data.forEach((room) => {
                 const li = document.createElement('li');
-                li.textContent = room_id;
+                li.textContent = room.room_number; // Доступ к полю room_number
                 roomsList.appendChild(li);
             });
         } catch (error) {
+            console.error('Error:', error);
             alert('Произошла ошибка при получении данных.');
         }
     });
