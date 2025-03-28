f368160ecf6cea6a65704b97cb7f9131a4477c08
Stepan <stepak038@gmail.com>
2025-03-28 22:19:47 +0800

    refactor

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from booking import views
 urlpatterns = [
- path('', views.home, name = "home"),
  path("signup/", views.SignUp.as_view(), name="signup"),
-path('logout/', views.LogoutView.as_view(), name='logout'),
+ path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
 ]
