98e93293b7ec5b282a52eb05f3d41d46f6d8e2c7
Stepan <stepak038@gmail.com>
2025-03-28 22:20:15 +0800

    refactor

--- hotelBooking_v2/urls.py
+++ hotelBooking_v2/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from booking.views import get_index_view, index, create_booking, booking_success, check_rooms
+from booking.views import get_index_view, index, create_booking, booking_success, check_rooms, booking_list_view
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path("", include('users.urls')),
     path("info", get_index_view, name="customer_info"),
-    path("index/", index, name="index"),
     path("booking", create_booking, name="create_booking"),
     path("booking/success/<int:booking_id>", booking_success, name="booking-success"),
+    path("booking/booking_manager/", booking_list_view, name="booking-manager"),
     path('check_rooms/', check_rooms),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('home/', include('users.urls')),
 ]
