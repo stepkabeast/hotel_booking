3f9770a8a214bc7862e6b5bbc4a290c27490f71b
Stepan <stepak038@gmail.com>
2025-03-28 22:19:41 +0800

    refactor

--- hotelBooking_v2/settings.py
+++ hotelBooking_v2/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-LOGIN_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "login"
 
 # Default primary key field type
